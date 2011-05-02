2e0dbdf4d69fa96cedc95b2eadac1a3498f38a90
Magnar Sveen <magnars@gmail.com>
2011-05-02 14:50:45 +0200

    simple router

--- test/test_router.js
+++ test/test_router.js
@@ -2,10 +2,23 @@
 /*globals module*/
 
 var testCase = require('nodeunit').testCase;
+var sinon = require('sinon');
+var router = require('router');
 
 module.exports = testCase({
-  "should do nothing": function (test) {
-    test.ok(true);
-    test.done();
+  setUp: function (callback) {
+    this.router = router.create();
+    callback();
+  },
+  
+  "should do routing asynchronously": function (test) {
+    var destination = sinon.stub();
+    this.router.addRoute("/", destination);
+    this.router.route("/");
+    test.ok(!destination.called);
+    process.nextTick(function () {
+      test.ok(destination.called);
+      test.done();
+    });
   }
 });

05ac07617c2b4643bd42a11bd6a866fe43adc4ee
Magnar Sveen <magnars@gmail.com>
2011-05-03 21:23:45 +0200

    use testCase

--- test/test_router.js
+++ test/test_router.js
@@ -2,15 +2,23 @@
 /*globals module*/
 
 require('sinon-nodeunit');
-var router = require('router').create();
+var testCase = require('sinon-nodeunit/deps/nodeunit').testCase;
+var router = require('router');
 
-exports["should do routing asynchronously"] = function (test) {
-  var destination = test.stub();
-  router.addRoute("/", destination);
-  router.route("/");
-  test.notCalled(destination);
-  process.nextTick(function () {
-    test.called(destination);
-    test.done();
-  });
-};
+module.exports = testCase({
+  setUp: function (callback) {
+    this.router = router.create();
+    callback();
+  },
+  
+  "should do routing asynchronously": function (test) {
+    var destination = test.stub();
+    this.router.addRoute("/", destination);
+    this.router.route("/");
+    test.notCalled(destination);
+    process.nextTick(function () {
+      test.called(destination);
+      test.done();
+    });
+  }
+});

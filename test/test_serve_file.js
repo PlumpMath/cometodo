996e926e2227024036feaee1382100636ad1ac35
Magnar Sveen <magnars@gmail.com>
2011-05-10 15:57:02 +0200

    file server had no state, changed to function

--- test/test_serve_file.js
+++ test/test_serve_file.js
@@ -8,14 +8,14 @@
   var testCase = require('sinon-nodeunit/deps/nodeunit').testCase;
   var yields = require('yields');
 
-  var file_server = require('file_server');
+  var serveFile = require('serve_file');
   var path = require('path');
   var fs = require('fs');
 
   module.exports = testCase({
     "should look for files in public folder": function (test) {
       test.stub(path, 'exists');
-      this.file_server.serve('/index.html');
+      serveFile('/index.html');
       test.calledWith(path.exists, './public/index.html');
       test.done();
     },
@@ -23,67 +23,67 @@
     "file found: should read as binary": function (test) {
       yields(path, 'exists', [true]);
       test.stub(fs, 'readFile');
-      this.file_server.serve('/index.html', null);
+      serveFile('/index.html', null);
       test.calledWith(fs.readFile, './public/index.html', 'binary');
       test.done();
     },
     
     "file found: should respond with 200 OK": function (test) {
       var response = createDummyResponse(test);
-      this.serveExistingFile(response, [null, 'file']);
+      serveExistingFile(response, [null, 'file']);
       test.calledWith(response.writeHead, 200);
       test.done();
     },
     
     "file found: should respond with contents in binary": function (test) {
       var response = createDummyResponse(test);
-      this.serveExistingFile(response, [null, 'file']);
+      serveExistingFile(response, [null, 'file']);
       test.calledWith(response.write, 'file', 'binary');
       test.done();
     },
     
     "file found: should end response": function (test) {
       var response = createDummyResponse(test);
-      this.serveExistingFile(response, [null, 'file']);
+      serveExistingFile(response, [null, 'file']);
       test.called(response.end);
       test.done();
     },
 
     "file not found: should respond with 404": function (test) {
-      var response = this.requestUnknownFile(test);
+      var response = requestUnknownFile(test);
       test.calledWith(response.writeHead, 404);
       test.done();
     },
 
     "file not found: should write 404 error message": function (test) {
-      var response = this.requestUnknownFile(test);
+      var response = requestUnknownFile(test);
       test.calledWith(response.write, '404 Not Found: ./public/404.html');
       test.done();
     },
 
     "file not found: should end response": function (test) {
-      var response = this.requestUnknownFile(test);
+      var response = requestUnknownFile(test);
       test.called(response.end);
       test.done();
     },
     
     "can't read file: should respond with 500 internal server error": function (test) {
       var response = createDummyResponse(test);
-      this.serveExistingFile(response, ['error', null]);
+      serveExistingFile(response, ['error', null]);
       test.calledWith(response.writeHead, 500);
       test.done();
     },
 
     "can't read file: should respond with contents": function (test) {
       var response = createDummyResponse(test);
-      this.serveExistingFile(response, ['error', null]);
+      serveExistingFile(response, ['error', null]);
       test.calledWith(response.write, 'error');
       test.done();
     },
     
     "can't read file: should end response": function (test) {
       var response = createDummyResponse(test);
-      this.serveExistingFile(response, ['error', null]);
+      serveExistingFile(response, ['error', null]);
       test.called(response.end);
       test.done();
     },
@@ -93,9 +93,6 @@
         path_exists: path.exists,
         fs_readFile: fs.readFile
       };
-      this.file_server = file_server.create();
-      this.serveExistingFile = serveExistingFile;
-      this.requestUnknownFile = requestUnknownFile;
       callback();
     },
 
@@ -117,13 +114,13 @@
   function serveExistingFile(response, read_file_result) {
     yields(path, 'exists', [true]);
     yields(fs, 'readFile', read_file_result);
-    this.file_server.serve('/index.html', response);
+    serveFile('/index.html', response);
   }
   
   function requestUnknownFile(test) {
     var response = createDummyResponse(test);
     yields(path, 'exists', [false]);
-    this.file_server.serve('/404.html', response);
+    serveFile('/404.html', response);
     return response;
   }
   

8c548e97ddfccfe17d9ae4049ab81fb1a597a5c2
Magnar Sveen <magnars@gmail.com>
2011-05-11 16:14:07 +0200

    refactored templates

---
+++ test/test_templates.js
@@ -0,0 +1,49 @@
+/*jslint indent: 2*/
+/*globals module, require*/
+
+require('sinon-nodeunit');
+var testCase = require('sinon-nodeunit/deps/nodeunit').testCase;
+var templates = require('templates');
+
+module.exports = testCase({
+  setUp: function (callback) {
+    this.templates = templates.create('./test/resources/');
+    callback();
+  },
+  
+  "should replace mustaches": function (test) {
+    var obj = {name: 'World'};
+    test.equals('Hello World!', this.templates.render('Hello {{name}}!', obj));
+    test.done();
+  },
+  
+  "should call render on objects": function (test) {
+    var obj = {item_collection: {
+      render: function () { return "<li><li><li>"; }
+    }};
+    test.equals('<ul><li><li><li></ul>',
+                this.templates.render('<ul>{{item_collection}}</ul>', obj));
+    test.done();
+  },
+
+  "should find full path": function (test) {
+    test.equals('./test/resources/template.html.tmpl', 
+                this.templates.pathTo('template'));
+    test.done();
+  },
+
+  "should load file": function (test) {
+    this.templates.load('template', {}, function (err, html) {
+      test.equals('Hello {{name}}!', html);
+      test.done();
+    });
+  },
+
+  "load should fail properly": function (test) {
+    this.templates.load('bad_file_name', {}, function (err, html) {
+      test.ok(err);
+      test.done();
+    });
+  },
+  
+});

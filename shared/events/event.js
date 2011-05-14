b8afa3199d5f0e5501c95101feaa87cf0d0547f6
Magnar Sveen <magnars@gmail.com>
2011-05-14 14:00:33 +0200

    hybridized event.js

--- shared/events/event.js
+++ shared/events/event.js
@@ -1,15 +1,25 @@
-var _ = require('underscore');
+var CT = this.CT || {};
 
-module.exports = {
-  create: function (members) {
-    return _.extend(Object.create(this), members);
-  },
-  
-  serialize: function () {
-    return JSON.stringify(this);
-  },
-  
-  deserialize: function (serialized) {
-    return _.extend(Object.create(this), JSON.parse(serialized));
-  }
+if (typeof module == "object" && typeof require == "function") {
+  var _ = require('underscore');
 }
+
+(function () {
+  CT.event = {
+    create: function (members) {
+      return _.extend(Object.create(this), members);
+    },
+
+    serialize: function () {
+      return JSON.stringify(this);
+    },
+
+    deserialize: function (serialized) {
+      return _.extend(Object.create(this), JSON.parse(serialized));
+    }
+  };
+
+  if (typeof module == "object") {
+    module.exports = CT.event;
+  }
+}());

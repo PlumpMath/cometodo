ce0ed3aaee139fcf625a660ce44a67ae67521456
Magnar Sveen <magnars@gmail.com>
2011-05-19 13:57:05 +0200

    moved /application.js to routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
 var http = require('http');
 var router = require('./routes').router;
 var serveFile = require('./serve_file');
-var serveClientScripts = require('./serve_client_scripts');
 var setContentType = require('./set_content_type');
 
 router.setDefault(serveFile);
-
-router.addRoute('/application.js', serveClientScripts);
 
 http.createServer(function (request, response) {
   setContentType(request, response);
--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,9 @@
 
 var router = require('./router').create();
 var redirect = require('./redirect');
+var serveClientScripts = require('./serve_client_scripts');
+
+router.addRoute('/application.js', serveClientScripts);
 
 router.addRoute("/", function (request, response) {
   redirect("/index.html", response);
